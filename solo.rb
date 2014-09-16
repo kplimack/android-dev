888effb44feb0b95c2a733a06319525fb707009c
Eric Internicola <einternicola@silverchalice.com>
2014-09-15 20:27:43 -0600

    Updated the solo.rb (working with Jake) and updated the README.md to show how to run the chef-solo.  Note: there is more to be done after running the chef solo to get the project running.

---
+++ solo.rb
@@ -0,0 +1,6 @@
+cookbook_path	["/Users/einternicola/code/android-dev/cookbooks",
+		"/Users/einternicola/code/android-dev/",
+		"/Users/einternicola/code/brothersmagoo-chef/cookbooks"
+		]
+role_path "/Users/einternicola/code/android-dev/roles"
+verbose_logging	true
