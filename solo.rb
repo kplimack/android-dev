6a1463ccaebe5541c2a1af39add14d42ede4ea88
Jake Plimack <jake.plimack@gmail.com>
2014-09-17 10:03:15 -0600

    make the solo.rb generic

--- solo.rb
+++ solo.rb
@@ -1,6 +1,19 @@
-cookbook_path	["/Users/einternicola/code/android-dev/cookbooks",
-		"/Users/einternicola/code/android-dev/",
-		"/Users/einternicola/code/brothersmagoo-chef/cookbooks"
-		]
-role_path "/Users/einternicola/code/android-dev/roles"
-verbose_logging	true
+
+
+
+# If these dirs don't work for you, just set ENV['COOKBOOK_PATH'] to point where you need it to.
+
+paths = ENV['COOKBOOK_PATH'] || Array.new
+%w{ chef/cookbooks repos/chef repos/android-dev }.each do |cb_path|
+  paths << ENV['HOME'] + '/' + cb_path
+end
+
+
+cookbook_path paths.map! { |path| "#{path}/cookbooks" } + Chef::Config[:cookbook_path]
+role_path paths.map! { |path| "#{path}/roles" } + Chef::Config[:cookbook_path]
+environment_path paths.map! { |path| "#{path}/environments" } + Chef::Config[:cookbook_path]
+data_bag_path paths.map! { |path| "#{path}/data_bags" } + Chef::Config[:cookbook_path]
+puts "COOKBOOK_PATH: " + cookbook_path.inspect
+puts "ROLE_PATH: " + role_path.inspect
+puts "ENV_PATH: " + environment_path.inspect
+puts "DB_PATH: " + data_bag_path.inspect
