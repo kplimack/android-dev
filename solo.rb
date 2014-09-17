9156d367ebe53d7d7fb54a18c172275507a4de02
Jake Plimack <jake.plimack@gmail.com>
2014-09-17 10:04:02 -0600

    rm debugging

--- solo.rb
+++ solo.rb
@@ -13,7 +13,3 @@ cookbook_path paths.map! { |path| "#{path}/cookbooks" } + Chef::Config[:cookbook
 role_path paths.map! { |path| "#{path}/roles" } + Chef::Config[:cookbook_path]
 environment_path paths.map! { |path| "#{path}/environments" } + Chef::Config[:cookbook_path]
 data_bag_path paths.map! { |path| "#{path}/data_bags" } + Chef::Config[:cookbook_path]
-puts "COOKBOOK_PATH: " + cookbook_path.inspect
-puts "ROLE_PATH: " + role_path.inspect
-puts "ENV_PATH: " + environment_path.inspect
-puts "DB_PATH: " + data_bag_path.inspect
